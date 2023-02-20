a0d0b312765dfbe6fac419f5a825f1745e1ca724
Alen Valdez <116237490+alenkvaldez@users.noreply.github.com>
2023-02-19 18:34:51 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQEX2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var games = new List<string>() { "NBA2K", "COD", "FIGHTNIGHT" };
+
+        var asc = games.OrderBy(games => games.Length);
+        foreach (var game in asc)
+        {
+            Console.WriteLine(game);
+        }
+    }
+}
+
+////Create a list of video game names...
+//Order the list of games by length of the game name.
+//Use the lambda expression in this exercise as well.
+//use Method Syntax for this exercise
